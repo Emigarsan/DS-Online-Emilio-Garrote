b8c07bb811e81a14a0e53b032c29ea9495d85547
Emilio Garrote <57704369+Emigarsan@users.noreply.github.com>
2026-03-11 12:51:39 +0100

    Cambios formato de cadenas para que sea más compacto

---
+++ Sprints/01_Programacion_Basica/Sprint_01/Unidad_02_Python_I/03_Practica_Obligatoria/programa.py
@@ -0,0 +1,4 @@
+print("\nBuenas tardes, bienvenido al servicio de pedido online,"
+ " el precio de nuestra pizzas es de 8,95€ para la familiar o de 9,90€ para la mediana")
+pizz_familiar=int(input("¿Cuántas pizzas  familiares desea?"))
+pizz_mediana=int(input("Estupendo, ¿cuántas pizzas medianas desea?"))
